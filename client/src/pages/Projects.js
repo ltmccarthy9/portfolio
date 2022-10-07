2d9e8e4bc27945699235cceaf58498f8ce8336b2
Liam McCarthy <ltmccarthy9@gmail.com>
2022-10-07 13:41:27 -0500

    project card margins

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,21 +2,34 @@
 const Projects = () => {
     return(
         <div>
-            <div className="container bio">
-            <h2>Appointment Manager</h2>
+            <h2 className="projects">Projects</h2>
+            <div className="container proj">
+            <h2>Appointment Manager (group)</h2>
             <p>Full stack application of a mock hospital.  User can sign up
                 and login to create, view, and delete appointments at their
                 department of need and their doctor of choice at a specific time.
+                I was responsible for the login/sign up as well as the appointment creation
+                after selection of a department.
             </p>
         </div>
-        <div className="container bio">
+        <div className="container proj">
             <h2>Ecommerce Backend</h2>
+            <p>
+                Backend, command line application of a mock Ecommerce database. Create, update,
+                and delete items in the database.
+            </p>
         </div>
-        <div className="container bio">
-            <h2>Friends First</h2>
+        <div className="container proj">
+            <h2>Friends First (Development)</h2>
+            <p>My personal take on a dating application.  Started the project
+                solo but recently added a few team members for collaboration.
+            </p>
         </div>
-        <div className="container bio">
-            <h2>Bokuchan's</h2>
+        <div className="container proj">
+            <h2>Bokuchan's (development)</h2>
+            <p>Freelance project that I'm currently working on for a 
+                restaurant/sauce business.
+            </p>
         </div>
         </div>
         
