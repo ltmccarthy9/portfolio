3ca1a138d4dfeefb60fce0ef6eac6c1f43278074
Liam McCarthy <ltmccarthy9@gmail.com>
2022-10-07 12:56:15 -0500

    transitions/style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,10 @@
 const Home = () => {
     return(
         <div className="container bio">
+            <h2>About</h2>
             <p>
                 Full stack web developer with a degree in economics. I recently earned a certificate
-             in full stack  web development from a bootcamp by the University of California - Berkeley.  
+             in full stack  web development from a boot camp by the University of California - Berkeley.  
              As a graduate of this bootcamp, I have over a hundred hours of experience building full-stack 
              applications both individually and in teams.  Beyond Javascript and HTML5/CSS, I am experienced in React, 
              mySQL, mongoDB, REST APIs, ORMs, git workflow, and more. I am a quick learner with strong analytical skills 
